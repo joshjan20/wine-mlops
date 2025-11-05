08314f5a59d0a69faabf7f746ed6161c14372211
JJ <airtoshh@gmail.com>
2025-11-05 18:49:20 +0100

    update

--- src/app.py
+++ src/app.py
@@ -3,13 +3,36 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 
+mlflow.set_tracking_uri("http://23.22.232.131:5000")
+EXPERIMENT_NAME = "Wine-Classification"
+
 # Load model from MLflow Model Registry or local folder
 MODEL_PATH = "models/wine_model"  # local path
 # or from MLflow Registry:
 # MODEL_PATH = "models:/wine-classifier/Production"
 
-print("🚀 Loading model from:", MODEL_PATH)
-model = mlflow.pyfunc.load_model(MODEL_PATH)
+#print("🚀 Loading model from:", MODEL_PATH)
+#MODEL_PATH = "s3://mlflow-artifacts-john/wine_model"  # MLflow artifact location
+#model = mlflow.pyfunc.load_model(MODEL_PATH)
+
+# Load latest model dynamically
+experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
+if experiment is None:
+    raise Exception(f"Experiment {EXPERIMENT_NAME} not found!")
+
+# Get the latest run
+runs = mlflow.search_runs(experiment_ids=[experiment.experiment_id],
+                          order_by=["start_time DESC"],
+                          max_results=1)
+
+if len(runs) == 0:
+    raise Exception("No runs found in the experiment")
+
+latest_run_id = runs.iloc[0]["run_id"]
+MODEL_URI = f"runs:/{latest_run_id}/wine_model"
+
+print("🚀 Loading latest model from:", MODEL_URI)
+model = mlflow.pyfunc.load_model(MODEL_URI)
 
 app = FastAPI(title="Wine Quality Prediction API")
 
